fe83375d026a2541be6f71276f87bf9ad589790b
rickyyao <rickkyao@xiaoe-tech.com>
2020-06-17 19:08:19 +0800

    处理上传

--- app/src/main/java/com/ricky/plugin/MainActivity.kt
+++ app/src/main/java/com/ricky/plugin/MainActivity.kt
@@ -6,6 +6,9 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.ricky.plugin_gson.GsonJarTransform
+import com.ricky.plugin_gson.GsonPlugin
+import com.ricky.plugin_gson_sdk.GsonPluginUtil
 
 class MainActivity : AppCompatActivity() {
 
